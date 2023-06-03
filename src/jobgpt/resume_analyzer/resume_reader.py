ad8a5d373002488f66d6ac8d0199d98d13d4d724
Kofi Wang <kofiwang2614@gmail.com>
2023-06-02 20:48:12 -0400

    feat: make resume reader class

--- src/jobgpt/resume_analyzer/resume_reader.py
+++ src/jobgpt/resume_analyzer/resume_reader.py
@@ -2,18 +2,23 @@
 
 import fitz
 
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    with fitz.open(pdf_path) as doc:
-        for page in doc:
-            text += page.get_text()
-    return text
+class ResumeReader:
+    def __init__(self):
+        pass
+    def extract_text_from_pdf(self, pdf_path):
+        text = ""
+        with fitz.open(pdf_path) as doc:
+            for page in doc:
+                text += page.get_text()
+        return text
 
-# Provide the path to your resume PDF file
-resume_path = 'data/resumes/resume_1.pdf'
+if __name__ == '__main__':
+    
+    # Provide the path to your resume PDF file
+    resume_path = 'data/resumes/resume_1.pdf'
 
-# Extract text from the resume PDF
-resume_text = extract_text_from_pdf(resume_path)
+    # Extract text from the resume PDF
+    resume_text = ResumeReader().extract_text_from_pdf(resume_path)
 
-# Print the extracted text
-print(resume_text)
+    # Print the extracted text
+    print(resume_text)
