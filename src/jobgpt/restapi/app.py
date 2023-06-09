8420f9a0d0d90b376b58e4fa39c0609313bc294c
Kofi Wang <kofiwang2614@gmail.com>
2023-06-08 21:53:45 -0400

    feat: add resume analyzer

--- src/jobgpt/restapi/app.py
+++ src/jobgpt/restapi/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
-
+from jobgpt.resume_analyzer.resume_analyzer import ResumeAnalyzer
 app = Flask(__name__)
+
+
 
 @app.route('/', methods=['GET'])
 def form():
@@ -12,10 +14,15 @@ def process_strings():
     # Extract strings from the form data
     resume = request.form.get('resume')
     job_description = request.form.get('job_description')
-
+    analyzer = ResumeAnalyzer()
+    output = analyzer.analyze(resume, job_description)
     # You can now do whatever you need with the strings
     # For now, we'll just return them in a plain text response
-    return f'resume: {resume}\n resume: {job_description}'
+    evaluation = output['evaluation']
+    suggestion = output['suggestions']
+    revised_resume = output['revised_resume']
+    return render_template('result.html', evaluation=evaluation, suggestion=suggestion, original_resume=resume, revised_resume=revised_resume)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
