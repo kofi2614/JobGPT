8db556dd609ffc6fe3e76499a083bc0ae1fd6ec6
Kofi Wang <kofiwang2614@gmail.com>
2023-06-29 23:21:15 -0400

    feat: revise resume segmenter

--- src/jobgpt/resume_analyzer/resume_segmenter.py
+++ src/jobgpt/resume_analyzer/resume_segmenter.py
@@ -26,6 +26,7 @@ When you are asked to provide evaluation or suggestion, make sure your are criti
 Focus on the use of professional language and the relevancy to the job description.
 REMEMBER DO NOT make things up or create fake experiences. 
 """.strip()
+
 user_teamplate = """
 You are an experienced career consultalt helping clients with their resumes.
 First, let's understand the client's background by reading the resume. 
@@ -33,6 +34,7 @@ Your job is to read the resume and segment the resume into different sections.
 A general resume should at least have work experience and education sections. 
 It may also have additional sections such as personal projects, summary and skills.
 Segment the given resume into the sections mentioned above.
+If you think that one section is missing, just DO NOT include the segment key in the output
 {json_format}
 
 resume: {resume_text}
@@ -43,7 +45,7 @@ class ResumeSegmenter:
         system_prompt = SystemMessagePromptTemplate.from_template(system_template)
         user_prompt = HumanMessagePromptTemplate.from_template(user_teamplate)
         resume_segmenter_prompt = ChatPromptTemplate(input_variables=["json_format", "resume_text"], messages=[system_prompt, user_prompt])
-        chain_segmenter= LLMChain(llm=openai, prompt=resume_segmenter_prompt)
+        self.chain_segmenter= LLMChain(llm=openai, prompt=resume_segmenter_prompt)
     def segment(self, resume_text: str):
         output = count_tokens(
             self.chain_segmenter,
@@ -52,7 +54,7 @@ class ResumeSegmenter:
                 "json_format": parser.get_format_instructions()
             }
         )
-        output_dict = json.loads(output['results'])
+        output_dict = json.loads(output['result'])
         output_dict = SegmentedResume(**output_dict)
         return output_dict
         
--- src/jobgpt/utils/dataclasses.py
+++ src/jobgpt/utils/dataclasses.py
@@ -4,14 +4,14 @@ from typing import List, Optional
 class WorkExperience(BaseModel):
     title: str = Field(description="The title of the position")
     company: str = Field(description="The company of the position")
-    bullet_points: List[str] = Field(description="The list of text that describes the experience")    
+    bullet_points: List[str] = Field(description="The list of text that describes the experience")
 
 class Education(BaseModel):
     school: str = Field(description="The school of the education")
     degree: str = Field(description="The degree of the education")
     major: str = Field(description="The major of the education")
     bullet_points: List[str] = Field(description="The list of text that describes the education")
-    
+
 class PersonalProject(BaseModel):
     title: str = Field(description="The title of the project")
     bullet_points: List[str] = Field(description="The list of text that describes the project and the canditate's contribution")
@@ -20,7 +20,7 @@ class Summary(BaseModel):
     text: List[str] = Field(description="The summary or highlight of the resume which usually appears at the top or buttom of the resume")
 
 class Skills(BaseModel):
-    text: str = Field(description="The list of skills that the candidate has")
+    text: List[str] = Field(description="The list of skills that the candidate has")
 
 class SegmentedResume(BaseModel):
     work_experiences: List[WorkExperience] = Field(description="The work experience section of the resume")
