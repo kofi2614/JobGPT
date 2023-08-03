7a842b8e47596a72a5df3592b5dc248188bfff3a
Kofi Wang <kofiwang2614@gmail.com>
2023-08-02 23:30:48 -0400

    feat: make sure that json string can be parsed in html

--- src/jobgpt/restapi/app.py
+++ src/jobgpt/restapi/app.py
@@ -10,9 +10,6 @@ import json
 
 app = Flask(__name__)
 
-with open('local/analyzed_resume.json', 'r') as f:
-    segmented_resume = json.load(f)  
-
 @app.route('/', methods=['GET'])
 def form():
     # Render the form template
@@ -27,27 +24,16 @@ def process_resumes():
     if not file or file.filename == '':        
         return 'No Resume Found'
     jd = json.dumps(process_text_for_html(request.form['job_description']))
-    byte_stream = io.BytesIO(file.stream.read())
-    # resume = ResumeReader().read(byte_stream)
-    # analyzer = ResumeAnalyzer()
-    # segmented_resume = ResumeSegmenter("gpt-4").segment(resume).to_list()    
-    # for resume_segment in segmented_resume:
-    #     if resume_segment:
-    #         resume_segment['analysis'] = analyzer.analyze(resume_segment, jd)                    
-    # print(segmented_resume)    
-    print("start working")
+    byte_stream = io.BytesIO(file.stream.read())        
     processed_resume = asyncio.run(ResumeProcessor().process(byte_stream, jd))    
     with open('local/processed_resume.json', 'w') as f:
         json.dump(processed_resume, f, indent=4)
     for segment in processed_resume:
         if segment:
             segment['analysis'] = process_text_for_html(segment['analysis'])
-            segment['content'] = process_text_for_html(segment['content'])
-            # segment['analysis'] = segment['analysis'].replace('\n', '\\n')
-            # segment['analysis'] = segment['analysis'].replace("""\"""", '')
-            # segment['content'] = segment['content'].replace('\n', '\\n')
-            # segment['content'] = segment['content'].replace("""\"""", '')
-    processed_resume_str = json.dumps(processed_resume)
+            segment['content'] = process_text_for_html(segment['content'])           
+    processed_resume_str = json.dumps(processed_resume)   
+    print(processed_resume_str)
     return render_template('segmented_resume.html', segmented_resume=processed_resume_str, jd=jd)                          
 
 @app.route('/followup', methods=['POST'])
@@ -69,4 +55,4 @@ def followup():
     return jsonify({"analysis": new_analysis})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
